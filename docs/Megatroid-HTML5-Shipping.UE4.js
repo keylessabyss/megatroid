2f17044512c7d46a4e47743b2adabce34fd964c0
Abielon <lushen.abdiel@gmail.com>
2026-07-03 17:16:02 -0500

    1.1

--- docs/Megatroid-HTML5-Shipping.UE4.js
+++ docs/Megatroid-HTML5-Shipping.UE4.js
@@ -75,7 +75,7 @@ var Module = {
 	assetDownloadProgress: {}, // Track how many bytes of each needed asset has been downloaded so far.
 
 	UE4_indexedDBName: 'UE4_assetDatabase_Megatroid', // this should be an ascii ID string without special characters that is unique to the project that is being packaged
-	UE4_indexedDBVersion: 202606292211, // Bump this number to invalidate existing IDB storages in browsers.
+	UE4_indexedDBVersion: 202607032153, // Bump this number to invalidate existing IDB storages in browsers.
 };
 
 
